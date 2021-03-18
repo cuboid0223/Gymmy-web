628b18be123590b9a68e9f37b6a20f1dad63ab25
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2021-03-18 20:58:59 +0800

    pick up v1 done

--- src/components/PickUpRobot/index.js
+++ src/components/PickUpRobot/index.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react";
 import SendIcon from "@material-ui/icons/Send";
 import { IconButton } from "@material-ui/core";
+import axios from "axios";
+
 const PickUpRobot = () => {
-  const [inputText, setInputText] = useState("");
-  const [sendText, setSendText] = useState([]);
-  const [robotText, setRobotText] = useState("");
-  //   console.log(userText);
+  const [isUser, setIsUser] = useState(true); // 還沒接後端 先用 true false 替代
+  const [inputText, setInputText] = useState(""); // 未傳送的訊息
+  const [sendText, setSendText] = useState([]); // 已傳送的訊息
+  const [messages, setMessages] = useState([]);
+  //   const [robotAnswers, setRobotAnswers] = useState([]);
+  console.log("sendText: ", sendText);
+
   const sendTextFunc = (e) => {
     e.preventDefault();
-    console.log("sendTextFunc");
     console.log("inputText: ", inputText);
-    setSendText([...sendText, inputText]);
-    // console.log("textArray: ", sendText?.textArray);
+    setSendText([...sendText, { message: inputText, user: isUser }]);
+    setMessages([...messages, { message: inputText, user: isUser }]);
+    console.log("messages: ", messages);
     console.log("sendText: ", sendText);
     setInputText(""); // clear input file
 
@@ -20,19 +25,61 @@ const PickUpRobot = () => {
     elem.scrollTop = elem.scrollHeight;
   };
   // request
+  useEffect(() => {
+    if (!sendText) {
+      return;
+    }
+    const options = {
+      method: "GET",
+      url:
+        "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer",
+      // 取得陣列最後一個元素，及最新的一條訊息
+      params: { q: sendText[sendText.length - 1]?.message },
+      headers: {
+        "x-rapidapi-key": "561ee6d36amshf73fd6455efaa12p1935aejsn0c7dc81a59b2",
+        "x-rapidapi-host":
+          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        const ans = response.data.answer;
+        const imageUrl = response.data.image;
+        setMessages([...messages, { answer: ans, imageUrl: imageUrl }]);
+        // setRobotAnswers([...robotAnswers, response.data.answer]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, [sendText]);
+  console.log("messages: ", messages);
   return (
     <div className="pickUpRobot">
       {/* message container  */}
       <div className="pickUpRobot__messageContainer" id="messageContainer">
-        {/* robot avatar */}
+        {/* robot default message */}
         <div className="pickUpRobot__robotText">
-          <p>Robot text</p>
+          <p>
+            Hey!
+            <br />
+            I am Pick-Up, you can ask me anything about 'nutrition'.
+            <br />
+            For example, How much vitamin c is in 2 apples?
+          </p>
         </div>
-        {/* user avatar */}
-        {/* input text */}
-        {sendText.map((text) => (
-          <div className="pickUpRobot__userText">
-            <p>{text}</p>
+        {/* user ask message */}
+        {messages.map(({ message, answer, user }) => (
+          <div
+            className={
+              //判斷訊息是否人為使用者輸入
+              user ? "pickUpRobot__userText" : "pickUpRobot__robotText"
+            }
+          >
+            <p className={!message && "message_display_none"}>{message}</p>
+            <p className={!answer && "message_display_none"}>{answer}</p>
           </div>
         ))}
       </div>
